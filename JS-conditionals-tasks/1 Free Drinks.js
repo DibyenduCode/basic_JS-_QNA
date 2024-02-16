58e85b646e8d8eba370eb887bd232c18e451046e
DibyenduCode <khaskeldibyendu22@gmail.com>
2024-02-16 23:33:34 +0530

    first commit

---
+++ JS-conditionals-tasks/1 Free Drinks.js
@@ -0,0 +1,16 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+var food=70;
+if(food>=500){
+    console.log("You Got a free Drink and You need to pay "+food);
+}
+else{
+    var totalPrice=food+30;
+    console.log("You need to pay "+totalPrice);
+    
+
+}

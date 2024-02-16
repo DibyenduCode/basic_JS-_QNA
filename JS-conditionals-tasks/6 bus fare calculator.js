afc5b6b6085f88fca07c70f9421e092e63eb7e3d
DibyenduCode <khaskeldibyendu22@gmail.com>
2024-02-17 01:01:37 +0530

    Last

--- JS-conditionals-tasks/6 bus fare calculator.js
+++ JS-conditionals-tasks/6 bus fare calculator.js
@@ -6,3 +6,23 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+var age=62;
+var work="job";
+var regual_Price=800;
+
+if(work=="student"){
+    var Discount=regual_Price*50/100;
+    var final=regual_Price-Discount;
+    console.log(final);
+}
+else if(age<10){
+    console.log("Free");
+}
+else if(age>=60){
+    var Discount=regual_Price*15/100;
+    var final=regual_Price-Discount;
+    console.log(final);
+}
+else{
+    console.log(regual_Price);
+}
